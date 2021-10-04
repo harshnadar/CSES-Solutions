e0c5d31537737d8bf060c1f4720c68d059012da3
Harsh Nadar <harshnadar.eee18@itbhu.ac.in>
2021-10-04 15:23:51 +0530

    Dynamic Programming

---
+++ Dynamic Programming/Book Shop.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+ 
+#define f(a,b,i) for(int i=a;i<b;i++)
+#define g(a,b,i) for(int i=b-1;i>=a;i--)
+#define int long long
+#define pb push_back
+#define FAST_IO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+// #define mp make_pair
+#define CIN(a,x,y) for(int i=x;i<y;i++) cin>>a[i];
+#define float double
+#define M 1000000007
+#define MAX INT_MAX
+#define MIN INT_MIN
+#define all(a) (a).begin(), (a).end()
+//#define N 1010
+// #define mp make_pair
+using namespace std;
+#define fi first
+#define sp(n) setprecision(n)
+#define se second
+#define in insert
+#define lb lower_bound
+#define ub upper_bound
+#define pi pair<int,int>
+#define bp(n) __builtin_popcount(n)
+#define MAXN 200005
+const int N=1e5+10;
+const int mod=1000000007;
+
+int n,x;
+vector<int> price;
+vector<int> pages;
+
+void solve() {
+    int n, x; cin>>n>>x;
+
+    price.resize(n); CIN(price,0,n)
+    pages.resize(n); CIN(pages,0,n)
+
+    vector<int> dp(x+1);
+
+    vector<int> dp1(x+1);
+    f(1,n+1,i){
+        f(1,x+1,j){
+            if(price[i-1]<=j){
+                dp[j] = max(pages[i-1]+dp1[j-price[i-1]], dp1[j]);
+            }
+            else{
+                dp[j] = dp1[j];
+            }
+        }
+        dp1=dp;
+    }
+
+    cout<<dp[x];
+}
+
+signed main(){
+    FAST_IO
+    #ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t; t=1;
+    
+    // cin>>t;
+    while(t--){
+        //solve();
+        solve();
+        //if(flag) return 0;
+    }
+}
