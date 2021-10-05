27cab65fd29c38ae41adc82ad147b774c19031ad
Harsh Nadar <harshnadar.eee18@itbhu.ac.in>
2021-10-05 10:14:19 +0530

    Dynamic Programming

---
+++ Dynamic Programming/Edit Distance.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+ 
+#define f(a,b,i) for(int i=a;i<b;i++)
+#define g(a,b,i) for(int i=b-1;i>=a;i--)
+#define int long long
+#define pb push_back
+#define FAST_IO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+// #define mp make_pair
+#define CIN(a,x,y) for(int i=x;i<y;i++) cin>>a[i];
+#define float double
+#define M 1000000007
+#define MAX INT_MAX
+#define MIN INT_MIN
+#define all(a) (a).begin(), (a).end()
+//#define N 1010
+// #define mp make_pair
+using namespace std;
+#define fi first
+#define sp(n) setprecision(n)
+#define se second
+#define in insert
+#define lb lower_bound
+#define ub upper_bound
+#define pi pair<int,int>
+#define bp(n) __builtin_popcount(n)
+#define MAXN 200005
+const int N=1e6;
+const int mod=1000000007;
+
+void solve() {
+    string a, b;
+    cin>>a>>b;
+
+    int n=a.size();
+    int m=b.size();
+
+    int dp[n+1][m+1];
+    memset(dp,0,sizeof dp);
+
+    f(0,n+1,i){
+        dp[i][0] = i;
+    }
+    f(0,m+1,i){
+        dp[0][i]=i;
+    }
+
+    f(1,n+1,i){
+        f(1,m+1,j){
+            if(a[i-1]==b[j-1]){
+                dp[i][j]=dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = 1+min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+            }
+        }
+    }
+
+    cout<<dp[n][m]<<endl;
+}
+
+signed main(){
+    FAST_IO
+    #ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t; t=1;
+    
+    // cin>>t;
+    while(t--){
+        //solve();
+        solve();
+        //if(flag) return 0;
+    }
+}
