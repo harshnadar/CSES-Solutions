5a6bc114a9199e8a95f1e21ee52b1ffa30e6cd93
Harsh Nadar <harshnadar.eee18@itbhu.ac.in>
2021-10-10 19:52:54 +0530

    Graphs

---
+++ Graph Algorithms/Longest Flight Route.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+ 
+#define f(a,b,i) for(int i=a;i<b;i++)
+#define g(a,b,i) for(int i=b-1;i>=a;i--)
+#define int long long
+#define pb push_back
+#define FAST_IO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+// #define mp make_pair
+#define CIN(a,x,y) for(int i=x;i<y;i++) cin>>a[i];
+#define float double
+#define M 1000000007
+#define MAX INT_MAX
+#define MIN INT_MIN
+#define all(a) (a).begin(), (a).end()
+//#define N 1010
+// #define mp make_pair
+using namespace std;
+#define fi first
+#define sp(n) setprecision(n)
+#define se second
+#define in insert
+#define lb lower_bound
+#define ub upper_bound
+#define pi pair<int,int>
+#define bp(n) __builtin_popcount(n)
+#define MAXN 200005
+const int N=1e5+2;
+const int mod=1000000007;
+const int INF = 1e17;
+const int NINF = -1*(INF);
+
+int in[N], par[N], dp[N];
+
+void solve() {
+    int n,m; cin>>n>>m;
+    vector<int> adj[n+1];
+    f(0,m,i){
+        int a,b;
+        cin>>a>>b;
+        adj[a].push_back(b);
+        in[b]++;
+    }
+    queue<int> q;
+
+    f(1,n+1,i){
+        if(in[i]==0) q.push(i);
+    }
+    
+    vector<int> v;
+
+    while(!q.empty()){
+        int cur = q.front();
+        q.pop();
+        v.pb(cur);
+
+        for(auto it: adj[cur]){
+            in[it]--;
+            if(in[it]==0) q.push(it);
+        }
+    }
+    memset(dp,-1,sizeof dp);
+
+    dp[1]=0;
+
+    for(auto i: v){
+        for(auto j: adj[i]){
+            if(dp[j]<dp[i]+1 && dp[i]!=-1){
+                dp[j]=dp[i]+1;
+                par[j]=i;
+            }
+        }
+    }
+
+    if(par[n]==0){
+        cout<<"IMPOSSIBLE\n";
+        return;
+    }
+    int j=n;
+    vector<int> ans;
+    while(par[j]){
+        ans.pb(j);
+        j=par[j];
+    }
+
+    ans.pb(1);
+    reverse(all(ans));
+
+    cout<<ans.size()<<endl;
+    for(auto it: ans){
+        cout<<it<<" ";
+    }
+}
+ 
+signed main(){
+    FAST_IO
+    // #ifndef ONLINE_JUDGE
+    // // for getting input from input.txt
+    // freopen("input.txt", "r", stdin);
+    // // for writing output to output.txt
+    // freopen("output.txt", "w", stdout);
+    // #endif
+    int t; t=1;
+    
+    // cin>>t;
+    while(t--){
+        //solve();
+        solve();
+        //if(flag) return 0;
+    }
+}
